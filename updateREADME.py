877f875f0ab342d38a1493cd5e26bd6e41b5e30c
YehyeokBang <qkddpgur318@gmail.com>
2023-05-19 21:59:29 +0900

    블로그 최신글 불러오기

--- updateREADME.py
+++ updateREADME.py
@@ -19,7 +19,7 @@ def make_latest_blog(url, conut):
                 url_slug = match.replace("\\", "")
                 if count >= 3:
                     break
-                latest_blog += "- [" + f"{url_slug}" + "]" + "(" + url + "/" + f"{url_slug}" + ")</br>"
+                latest_blog += "- [" + f"{url_slug}" + "]" + "(" + url + "/" + f"{url_slug}" + ")\n"
                 count += 1
             return latest_blog
         else:
@@ -29,32 +29,28 @@ def make_latest_blog(url, conut):
 
 def make_readme(latest_blogs):
     return f"""## Yehyeok Bang
- Hi I'm Yehyeok 👋</br>
+ Hi I'm Yehyeok 👋  
  Growing Backend Developer🌱
  
-<h3>I'm studying 📖</h3>
+### I'm studying 📖
 <div style="display:inline">
 <img src="https://img.shields.io/badge/Java-007396?style=for-the-badge&logo=OpenJDK&logoColor=white"/> 
 <img src="https://img.shields.io/badge/spring-6DB33F?style=for-the-badge&logo=spring&logoColor=white"> <img src="https://img.shields.io/badge/springboot-6DB33F?style=for-the-badge&logo=springboot&logoColor=white"> <img src="https://img.shields.io/badge/mysql-4479A1?style=for-the-badge&logo=mysql&logoColor=white"> <br />
 </div>
 
-<h3>Welcome my 🙌</h3>
+### Welcome my 🙌
 <a href="https://velog.io/@hyeok_1212" target="_blank"><img src="https://img.shields.io/badge/velog-99FFCC?style=for-the-badge&logo=VELOG&logoColor=003300"/></a>
 <a href="mailto:qkddpgur318@gmail.com"><img src="https://img.shields.io/badge/Gmail-D0A9F5?style=for-the-badge&logo=Gmail&logoColor=white&link=mailto:wonjongah@gmail.com"/></a>
 
-<h3>Algorithm ✏️</h3>
+### Algorithm ✏️
  
 [![Solved.ac Profile](http://mazassumnida.wtf/api/generate_badge?boj=aksk333)](https://solved.ac/aksk333)
 
-<!-- <h3>GitHub stats :seedling:</h3>
-
-![YehyeokBang's GitHub stats](https://github-readme-stats.vercel.app/api?username=YehyeokBang&show_icons=true&theme=gruvbox)
--->
-<h3>Most Used Languages 🥇</h3>
+### Most Used Languages 🥇
 
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=YehyeokBang&layout=compact&theme=dark)](https://github.com/jogilsang/jogilsang)  
 
-<h3>Latest Blogs 😄</h3>
+### Latest Blogs 😄
 {latest_blogs}
 
 """
@@ -63,7 +59,3 @@ if __name__ == "__main__":
     readme = make_readme(make_latest_blog(CONST_URL, CONST_COUNT))
     with open("./README.md", 'w', encoding='utf-8') as f:
         f.write(readme)
-
-
-
-
